ca069682ffaf99b2e6e508b1503a544adabb39bf
Hamed Najafi <h.n140490@gmail.com>
2026-09-17 23:30:25 +0330

    حذفیات

--- src/presentation/dtos/initial-operations/attachment-dto.ts
+++
@@ -1,8 +0,0 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsString } from "class-validator";
-
-export class AttachmentDto {
-    @ApiProperty({ description: 'File URL' })
-    @IsString()
-    fileUrl: string;
-}
--- src/presentation/helpers/resend-code-time.ts
+++
@@ -1,4 +0,0 @@
-export class ResendCodeTime{
-    isExist:boolean;
-    second:number|null
-}
